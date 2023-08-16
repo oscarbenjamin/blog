c4ab8c85d57e4b0c37df7d35b607391f553ccdcd
Oscar Benjamin <oscar.j.benjamin@gmail.com>
2023-08-16 13:43:30 +0100

    Add blog post

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'blog'
+copyright = '2023, Oscar Benjamin'
+author = 'Oscar Benjamin'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+extensions = [
+    'sphinx.ext.doctest',
+    'sphinx.ext.graphviz',
+]
+graphviz_output_format = 'svg'
---
+++ source/index.rst
@@ -0,0 +1,21 @@
+.. blog documentation master file, created by
+   sphinx-quickstart on Tue Jan 31 11:59:42 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Oscar Benjamin's blog
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   czi/index.rst
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
